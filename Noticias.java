0159e08c5f82e9d1e03e2fdd2f4d172a180303d1
incognite2349 <127636628+incognite2349@users.noreply.github.com>
2023-09-24 22:31:44 -0500

    Create Noticias.java
    Creacion de una ventana de noticias adentro de la app

---
+++ Noticias.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+//construir una ventana de noticias y de objetos de interes del usuario
+public class loggin {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {}
+    }
+}
