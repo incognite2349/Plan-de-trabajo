2b33d52c9614ffbd069be87a047f2e7e00cf8e2f
incognite2349 <127636628+incognite2349@users.noreply.github.com>
2023-09-24 22:22:20 -0500

    Create foro.java
    foro app

---
+++ foro.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class loggin {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {}
+      //foro de la aplicacion
+    }
+}
