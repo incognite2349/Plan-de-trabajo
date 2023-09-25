a2dc3c4abe276a8f16e65caf4d1a66503b5aac5d
incognite2349 <127636628+incognite2349@users.noreply.github.com>
2023-09-24 22:37:45 -0500

    Create Gustos.java
    Gustos del usuario

---
+++ Gustos.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+//creacion de una seccion de "recomendados" guiados por los gustos y prefencias del usuario
+public class loggin {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {}
+    }
+}
