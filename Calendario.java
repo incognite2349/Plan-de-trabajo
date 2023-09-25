b97c67ef1a185134ce166dea03bfe85e566b57a0
incognite2349 <127636628+incognite2349@users.noreply.github.com>
2023-09-24 22:34:26 -0500

    Create Calendario.java
    Creacion del calendario de eventos de la U

---
+++ Calendario.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+//Creacion de un calendario en el cual se enlisten los eventos proximos a ocurrir ademas de los detalles del mismo
+public class loggin {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {}
+    }
+}
