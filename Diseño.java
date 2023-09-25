087dc827f6a176701de9f69fd427dae9b52239ac
incognite2349 <127636628+incognite2349@users.noreply.github.com>
2023-09-24 22:24:50 -0500

    Create Diseño.java
    creacion de la estructura de la plataforma a crear

---
+++ Diseño.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+// creacion backend del diseño y aplicaciones no funcionales (estrutura de la app)
+public class loggin {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {}
+    }
+}
